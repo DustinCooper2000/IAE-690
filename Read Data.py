9423e4dce204d1db47c7b2f03ba53fc2f343cd0b
cooperd <cooperd@berea.edu>
2024-03-06 17:44:51 -0500

    Imported the cyrptpandas library to encrypt the dataframe. Encrypted the data frame and decrypted it to protect PII.

--- Read Data.py
+++ Read Data.py
@@ -4,6 +4,10 @@ import math
 import os
 import numpy as np
 import xlrd
+import cryptpandas as crp
 # Read the entire Excel file into a DataFrame
 df = pd.read_excel(r'./sample-data.xlsx', sheet_name="Sheet1")
-print(df)
+df = df.astype(str)
+crp.to_encrypted(df, "hello", r'./test')
+decrypted_df = crp.read_encrypted(r'./test', "hello")
+print(decrypted_df)
