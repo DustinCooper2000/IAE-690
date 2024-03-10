a2e9e70132f5d18c7f8c35bbf44d4d1c906313eb
cooperd <cooperd@berea.edu>
2024-03-10 14:15:00 -0400

    Added an environment file to hold our secret key for encryption so that it is not hardcoded into the .py file.

--- Read Data.py
+++ Read Data.py
@@ -5,9 +5,13 @@ import os
 import numpy as np
 import xlrd
 import cryptpandas as crp
+import dotenv
 # Read the entire Excel file into a DataFrame
 df = pd.read_excel(r'./sample-data.xlsx', sheet_name="Sheet1")
 df = df.astype(str)
+dotenv.load_dotenv(r'./env.env')
+SECRET_KEY = os.environ.get("Password")
 crp.to_encrypted(df, "hello", r'./test')
 decrypted_df = crp.read_encrypted(r'./test', "hello")
-print(decrypted_df)
+#print(decrypted_df)
+print(SECRET_KEY)
