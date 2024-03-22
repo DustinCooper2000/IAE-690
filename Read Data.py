41d8a91027e119201a62f2c6e7893eeeeddf41d7
cooperd <cooperd@berea.edu>
2024-03-22 17:41:05 -0400

    Added code to drop the SSN column as it is too valuable.

--- Read Data.py
+++ Read Data.py
@@ -3,11 +3,17 @@ import matplotlib
 import math
 import os
 import numpy as np
-import xlrd
+import random
 import cryptpandas as crp
 import dotenv
 import pyotp
 
+def Anonymize_Dataframe(dataframe):
+    df = dataframe
+    df.drop(columns = ["SSN"], inplace = True)
+    for i in range(len(df["first name"])):
+        df.loc[i, "first name"] = random.choice(firstnames)
+        df.loc[i, "last name"] = random.choice(lastnames)
 def simulate_authentication(key):
    # Simulate the process of authenticating with a TOTP code.
    totp = pyotp.TOTP(key)
@@ -16,8 +22,6 @@ def simulate_authentication(key):
    user_input = input("Enter Code: ")
    #if totp.verify(user_input):
    print("Authentication successful!")
-   df = pd.read_excel(r'./sample-data.xlsx', sheet_name="Sheet1")
-   df = df.astype(str)
    dotenv.load_dotenv(r'./env.env')
    SECRET_KEY = os.environ.get("Password")
    crp.to_encrypted(df, SECRET_KEY, r'./test')
@@ -32,7 +36,16 @@ def simulate_authentication(key):
         print("Authentication failed. Please try again with the right key.")
 
 # Main Code
+firstnames = ["Jim", "John", "Sue", "William", "Nathan", "Chris", "Ryan", "Sandy",
+              "Rachael", "Candis", "Billy", "Bob", "Kelly", "Brianna", "Rebecca",
+              "Randy", "Precious", "Barbara", "Heather"]
+lastnames = ["Price", "Smith", "King", "Mobley", "Arthur", "Reynolds", "Dion",
+             "Carpenter", "Back", "Jones", "Johnson", "Anderson", "Brown",
+             "Garcia", "Miller", "Thompson", "Jackson"]
 # The key should be the same one generated and used to create the QR code in Program 1
+df = pd.read_excel(r'./sample-data.xlsx', sheet_name="Sheet1")
+Anonymize_Dataframe(df)
+df = df.astype(str)
 user_key = open("2fa.txt").read()  # Reading the key from the file generated in Program 1 (otp_qrcode_and_key.py)
 simulate_authentication(user_key)
 # Read the entire Excel file into a DataFrame
